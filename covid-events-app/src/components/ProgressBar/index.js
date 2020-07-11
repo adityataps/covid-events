16377d37716a283de392147f67a865e1b9e72621
jchen14 <jesschen14@gmail.com>
2020-07-10 22:02:01 -0400

    Progress Bar is visible
    need to test if other pages can change the progress

--- covid-events-app/src/components/ProgressBar/index.js
+++ covid-events-app/src/components/ProgressBar/index.js
@@ -1,52 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div`
-    background-color: #111;
-    color: white;
-    padding: 16px;
-    z-index: 999;
-    transition: top 0.5s ease;
+const Progress = styled.div`
+    background-color: #d8d8d8;
+    border-radius: 20px;
+    position: relative;
+    margin: 15px 0;
+    height: 30px;
+    width: 300px;
 `;
 
-const progress = document.querySelector('.progress-done');
+const ProgressDone = styled.div`
+    background: linear-gradient(to left, #F2709C, #FF9472);
+    box-shadow: 0 3px 3px -5px #F2709C, 0 2px 5px #F2709C;
+    border-radius: 20px;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: ${props => props.width}%;
+    opacity: 1;
+    transition: 1s ease 0.3s;
+`;
 
+function updateProgress(int) {
+    this.setProgress({int})
+}
 
 export default class ProgressBar extends React.Component {
 
     constructor(props){
         super(props);
         this.state = {
-            right: 9999
+            progress: 0
         };
+        updateProgress = updateProgress.bind(this)
     }
 
     onShow = () => {
-
     }
 
-    // showNotification = () => {
-    //     this.setState({
-    //         right: 100
-    //     }, () => {
-    //         setTimeout(() => {
-    //             this.setState({
-    //                 right: 9999
-    //             })
-    //         }, 3000);
-    //     })
+    // showProgress = () => {
+    //     progress.style.width = progress.getAttribute('data-done') + '%';
+    //     progress.style.opacity = 1;
     // }
-
-    showProgress = () => {
-        progress.style.width = progress.getAttribute('data-done') + '%';
-        progress.style.opacity = 1;
-    }
 
     render(){
         return(
             <React.Fragment>
-                 <Container id={this.props.id} >This is your progress bar!</Container>
-             </React.Fragment>
+                 <Progress id={this.props.id} >
+                     <ProgressDone width={this.state.progress}></ProgressDone>
+                     Progress Bar
+                 </Progress>
+            </React.Fragment>
         );
     }
 }
