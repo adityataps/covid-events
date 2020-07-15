daaa1390f0d5e568567e5946c4f3de4ad2b4986f
micriley <micriley@gmail.com>
2020-07-14 21:36:25 -0400

    Fixed guestlist formatting

--- covid-events-app/src/components/screens/guestlist.js
+++ covid-events-app/src/components/screens/guestlist.js
@@ -19,9 +19,10 @@ const TitleBar = styled.h1`
 const ContentSection = styled.p`
     text-align: left;
     color: white;
-    font-size: 20px;
-    position: absolute;
-    height: 90%;
+    font-size: 10px;
+    position: relative;
+    width: 100%;
+    display:block;
     padding-left: 10px;
     padding-right: 10px;
     top: 40px;
@@ -51,6 +52,10 @@ const PrevButton = styled.button`
     position: absolute;
 `;
 
+const AgeInput = styled.input`
+    width:3%
+`;
+
 export class Guestlist extends React.Component {
     constructor(props){
         super(props);
@@ -62,8 +67,36 @@ export class Guestlist extends React.Component {
     render(){
         return(
             <Container>
+<<<<<<< Updated upstream
                 <TitleBar>Stuff about guestlist here!</TitleBar>
                 <ContentSection>Stuff about guestlist here!
+=======
+                <TitleBar>Who are you inviting?</TitleBar>
+                <ContentSection>
+                    Enter the name of Guest 1: <input type = "text" value = {this.state.name1} onChange = {this.updateName1} />
+                    Enter the age of Guest 1: <AgeInput type = "text" value = {this.state.age1} onChange = {this.updateAge1} />
+                    Enter the email of Guest 1: <input type = "text" value = {this.state.email1}onChange = {this.updateEmail1} />
+                </ContentSection>
+                <ContentSection>
+                        Enter the name of Guest 2: <input type = "text" value = {this.state.name2} onChange = {this.updateName2} />
+                        Enter the age of Guest 2: <AgeInput type = "text" value = {this.state.age2} onChange = {this.updateAge2} />
+                        Enter the email of Guest 2: <input type = "text" value = {this.state.email2} onChange = {this.updateEmail2} />
+                </ContentSection>
+                <ContentSection>
+                    Enter the name of Guest 3: <input type = "text" value = {this.state.name3} onChange = {this.updateName3} />
+                    Enter the age of Guest 3: <AgeInput type = "text" value = {this.state.age3} onChange = {this.updateAge3} />
+                    Enter the email of Guest 3: <input type = "text" value = {this.state.email3} onChange = {this.updateEmail3} />
+                </ContentSection>
+                <ContentSection>
+                    Enter the name of Guest 4: <input type = "text" value = {this.state.name4} onChange = {this.updateName4} />
+                    Enter the age of Guest 4: <AgeInput type = "text" value = {this.state.age4} onChange = {this.updateAge4} />
+                    Enter the email of Guest 4: <input type = "text" value = {this.state.email4} onChange = {this.updateEmail4} />
+                </ContentSection>
+                <ContentSection>
+                    Enter the name of Guest 5: <input type = "text" value = {this.state.name5} onChange = {this.updateName5} />
+                    Enter the age of Guest 5: <AgeInput type = "text" value = {this.state.age5} onChange = {this.updateAge5} />
+                    Enter the email of Guest 5: <input type = "text" value = {this.state.email5} onChange = {this.updateEmail5} />
+>>>>>>> Stashed changes
                 </ContentSection>
                 <Link to={"/location"}><NextButton>Next</NextButton></Link>
                 <Link to={"/attendees"}><PrevButton>Prev</PrevButton></Link>
